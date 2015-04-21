0e941be938f21b119c512be23639360d9d5920da
Julian Viereck <julian.viereck@gmail.com>
2015-04-22 00:24:40 +0200

    Adding missing virtual-css/buildCSSRule.js

---
+++ 01_basic_idea/virtual-css/buildCSSRule.js
@@ -0,0 +1,49 @@
+// Imported from:
+// https://github.com/martinandert/react-inline/blob/aef7dcd0da/src/buildCSSRule.js
+
+/*
+ * @providesModule buildCSSRule
+ */
+
+const isUnquotedContentValue = /^(normal|none|(\b(url\([^)]*\)|chapter_counter|attr\([^)]*\)|(no-)?(open|close)-quote|inherit)((\b\s*)|$|\s+))+)$/;
+const uppercaseLetter = /([A-Z])/g;
+
+const unitlessNumbers = {
+  boxFlex: true,
+  boxFlexGroup: true,
+  columnCount: true,
+  flex: true,
+  flexGrow: true,
+  flexPositive: true,
+  flexShrink: true,
+  flexNegative: true,
+  fontWeight: true,
+  lineClamp: true,
+  lineHeight: true,
+  opacity: true,
+  order: true,
+  orphans: true,
+  widows: true,
+  zIndex: true,
+  zoom: true,
+
+  // SVG-related
+  fillOpacity: true,
+  strokeDashoffset: true,
+  strokeOpacity: true,
+  strokeWidth: true
+};
+
+export default function buildCSSRule(key, value, options) {
+  if (!unitlessNumbers[key] && typeof value === 'number') {
+    value = '' + value + 'px';
+  } else if (key === 'content' && !isUnquotedContentValue.test(value)) {
+    value = "'" + value.replace(/'/g, "\\'") + "'";
+  }
+
+  return hyphenate(key) + ': ' + value + ';';
+}
+
+function hyphenate(string) {
+  return string.replace(uppercaseLetter, '-$1').toLowerCase();
+}
